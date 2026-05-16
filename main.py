b2881cd76539b6591919897c31e2e4a2591a8173
WinterLeaf <winterleaf1239@tutamail.com>
2026-05-16 23:30:20 +0300

    Lastcom

---
+++ main.py
@@ -0,0 +1,3 @@
+import proverka
+
+proverka.main()
--- proverka.py
+++ proverka.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import os
 
 class Obrabotka:
     def __init__(self,file_path):
@@ -7,31 +6,70 @@ class Obrabotka:
     
     def found(self):
         try:
-            df = pd.read_csv(self.file, encoding="utf-8")
-            
-            # Дополнительная проверка: если в файле есть только строка заголовков, 
-            if df.empty:
-                print(f"Файл '{self.file}' содержит только заголовки, но в нем нет данных.")
-                return False
-                
-            print('Все хорошо, файл найден и содержит данные.')
+            self.df = pd.read_csv(self.file, sep=',')
 
-            
         except FileNotFoundError:
-            print(f"Возникла следующая ошибка: [Errno 2] No such file or directory: '{self.file}'")
+            print(f"Возникла следующая ошибка: No such file or directory: '{self.file}'")
+     
+        except pd.errors.EmptyDataError:
+            print(f"Ошибка: Датафрейм {self.file} пуст.")
+
+        else:
+
+            try:
+                self.df = pd.read_csv(self.file, sep=',')
+                column = [ 'Участники гражданского оборота',   
+                            'Тип операции',
+                            'Сумма операции',
+                            'Вид расчета',
+                            'Место оплаты',
+                            'Терминал оплаты',
+                            'Дата оплаты',
+                            'Время оплаты',
+                            'Результат операции',
+                            'Cash-back',
+                            'Сумма cash-back']
+
+                if list(self.df.columns) != column:
+                    raise ValueError(f"Названия стобцов не совпадают.\nОжидаемые: {column}\nФактические: {list(self.df.columns)}")
+            
+                
+                type = {'Участники гражданского оборота' : 'str',
+                         'Тип операции' : 'str',
+                         'Сумма операции' : 'float64',
+                         'Вид расчета' : 'str',
+                         'Место оплаты' : 'str',
+                         'Терминал оплаты' : 'str',
+                         'Дата оплаты' : 'str',
+                         'Время оплаты' : 'str',
+                         'Результат операции' : 'str',
+                         'Cash-back' : 'str',
+                         'Сумма cash-back' : 'float64'}
+            
+                for i,true_type in type.items():
+                    our_type = str(self.df[i].dtypes)
+                    if our_type != true_type:
+                        raise TypeError(f"В столбце: '{i}' тип данных не соответствует оиждаемому\nОжидается: {true_type}, Фактически: {our_type}\n")
+
 
             
-        except pd.errors.EmptyDataError: #умный модуль, который сам проверяет
-            print(f"Ошибка: Файл '{self.file}' абсолютно пустой.")
+            except ValueError as e:
+                print(f"Возникла ошибка: {e} ")
 
-        
+            except TypeError as e:
+                print(f"Возникла ошибка: {e} ")
             
-
+            else: 
+                self.df = pd.read_csv(self.file, sep=',')
+                print('В датафрейме ошибок не найдено')
         
-x = Obrabotka('var10.csv')
-x.found()
 
 
 
+def main():
+    x = Obrabotka('var10.csv')
+    x.found()
+if __name__ == "__main__":
+    main()
 
-#except pd.errors.EmptyDataError: Этот блок сработает автоматически, если в файле физически нет ни одного символа или там записаны только невидимые пробелы/переносы строк.if df.empty: Эта проверка нужна на случай, если файл не пустой (в нем есть одна строка с названиями колонок), но самих строк с данными внутри не
+
